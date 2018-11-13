d041fa861566f30e51a85e86714f55a2f4cc6ef8
Sagar Ghimire <sagar@growcept.com>
2018-11-13 11:18:01 +1100

    Create New Branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
-import logo from './assets/dial.png'
-import ProgressArc from './progressArc';
 
 class App extends Component {
 
   state = {
-    percentComplete: 0.3,
+    muso: false
   }
 
-  togglePercent = () => {
-    const percentage = this.state.percentComplete === 0.3 ? 0.7 : 0.3
-    this.setState({ percentComplete: percentage })
+  hover = () => {
+    this.setState({
+      muso: !this.state.muso
+    })
   }
 
   render() {
-    console.log(logo)
     return (
       <div>
-        <ul class='circle-container text-center'>
+        <ul onMouseOver={this.hover} class='circle-container text-center'>
         <li>A</li>
         <li>B</li>
         <li>C</li>
