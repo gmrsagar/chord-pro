738e926eeec3d77f701f640b261a7b49946ecdaa
Sagar Ghimire <gmrsagar@gmail.com>
2019-01-16 18:13:00 +1100

    Updates

---
+++ precache-manifest.8a5a08fe672c822b5902d7362063d324.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f39f8f51ca78c789aa5e1a3abb3381f",
+    "url": "/chord-pro/static/media/dial.8f39f8f5.png"
+  },
+  {
+    "revision": "f70ae91f53edcebcf1a7",
+    "url": "/chord-pro/static/js/runtime~main.f70ae91f.js"
+  },
+  {
+    "revision": "b503c29d983901e6e5ec",
+    "url": "/chord-pro/static/js/main.b503c29d.chunk.js"
+  },
+  {
+    "revision": "e951900407267c0a117b",
+    "url": "/chord-pro/static/js/1.e9519004.chunk.js"
+  },
+  {
+    "revision": "b503c29d983901e6e5ec",
+    "url": "/chord-pro/static/css/main.366bf226.chunk.css"
+  },
+  {
+    "revision": "e951900407267c0a117b",
+    "url": "/chord-pro/static/css/1.3c452987.chunk.css"
+  },
+  {
+    "revision": "070c467d4d9471d0809afe531144e7ea",
+    "url": "/chord-pro/index.html"
+  }
+];
